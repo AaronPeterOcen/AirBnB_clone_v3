eb1018abb1fd04f451274f203b85c0628ef90963
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-02-26 20:16:14 +0300

    app_views not imported error

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 
-from models import storage 
-from api.v1.views import app_views 
+from models import storage
+from api.v1.views import app_views
 from os import getenv
 
 app = Flask(__name__)
@@ -12,9 +12,10 @@ app.register_blueprint(app_views)
 def close_db(obj):
     """calls methods close"""
     storage.close()
-    
+
+
 if __name__ == "__main__":
-    host = getenv('HBNB_API_HOST', default='0.0.0.0')
-    port = getenv('HBNB_API_PORT', default=5000)
+    host = getenv("HBNB_API_HOST", default="0.0.0.0")
+    port = getenv("HBNB_API_PORT", default=5000)
 
     app.run(host, int(port), threaded=True)
